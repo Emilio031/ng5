3119cdb9336f8dd7284d664488bc9848550ade99
KevinLi <KevinLi@Emilio-2.local>
2018-05-09 13:14:22 +0800

    login

---
+++ src/app/ApiService/api.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ApiService {
+  // api位置
+  baseUrl = '/Vote.api/api';
+
+  headers = new Headers();
+
+  constructor(private http: Http) {
+    this.headers.append('Content-Type', 'application/json');
+  }
+
+  /*  假如你的api request為 https://www.googleapis.com/youtube/v3
+        https://www.googleapis.com/ -> baseUrl
+        youtube/v3 -> url 參數 (post時不一定有) */
+
+  get<T>(url: string) {
+    console.log('header', this.headers);
+    return (<Observable<any>>this.http
+      .get(`${this.baseUrl + url}`, { headers: this.headers })
+      .map((res: Response) => res.json())
+      .catch(this.handleError)) as Observable<any>;
+  }
+  post<T>(url: string, body) {
+    console.log('header', this.headers);
+    return (<Observable<any>>this.http
+      .post(`${this.baseUrl + url}`, body, { headers: this.headers })
+      .map((res: Response) => res.json())
+      .catch(this.handleError)) as Observable<any>;
+  }
+
+  setToken(token: string) {
+    this.headers.append('Token', token);
+  }
+
+  handleError(res: Response) {
+    return Observable.throw(res);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
